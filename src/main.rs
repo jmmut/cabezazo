5799d30dbade593c51d767771c63e1d55f005794
jose miguel mut <jomutlo@gmail.com>
2023-03-26 16:06:04 +0200

    draw background and runner placeholder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Cabezazo")]
+async fn main() {
+
+    let runner_size = Vec2::new(64.0, 64.0);
+    let runner_pos = Vec2::new(screen_width()/2.0 - runner_size.x/2.0, screen_height() - runner_size.y);
+
+
+    loop {
+        clear_background(SKYBLUE);
+
+        // let delta = get_frame_time();
+        //
+        // if is_key_down(KeyCode::Right) && platform_x < SCR_W - platform_width / 2. {
+        //     platform_x += 3.0 * delta;
+        // }
+        // if is_key_down(KeyCode::Left) && platform_x > platform_width / 2. {
+        //     platform_x -= 3.0 * delta;
+        // }
+        draw_rectangle(runner_pos.x, runner_pos.y, runner_size.x, runner_size.y, WHITE);
+
+        next_frame().await
+    }
 }
